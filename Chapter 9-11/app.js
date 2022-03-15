4d5dc13dfaae7c2338818728c3c15649c41249a8
shaykhmirzaban <shaykhmirzaban@gmail.com>
2022-03-15 19:48:21 +0500

    JavaScript task 2

---
+++ Chapter 9-11/app.js
@@ -0,0 +1,13 @@
+// Chapter 6-11
+// ***********
+// USER INPUT & CONDITIONAL STATEMENT
+// **********************************
+
+// 1
+// *
+// Take input for user
+let cityName = prompt("Enter your city name","Karachi");
+// Using if statement for condition
+if(cityName === "Karachi" || cityName === "karachi"){
+    alert("Welcome to city of lights")
+}
